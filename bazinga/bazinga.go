96542fd6106e4d763edb442be13171a16dc107e8
William Gunnells <gunnells@gmail.com>
2022-06-19 00:03:39 -0700

    new info about workspaces

--- bazinga/bazinga.go
+++ bazinga/bazinga.go
@@ -2,12 +2,27 @@ package main
 
 import (
     "fmt"
-
+    "log"
     "example.com/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message := greetings.Hello("Gladys")
-    fmt.Println(message)
+    // Set properties of the predefined LOgger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // A slice of names.
+    names := []string{"Glady", "Samantha", "Darrin"}
+
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
+    if err != nil {
+        log.Fatal(err)
+    } 
+    // If no error was returned, print the returned message
+    // to the console.
+    fmt.Println(messages)
 }
+
