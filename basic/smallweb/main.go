067ae00a8b2d8fd7bc945500f98ab107485661b7
William Gunnells <gunnells@gmail.com>
2022-08-22 20:49:11 -0700

    templates

--- basic/smallweb/main.go
+++ basic/smallweb/main.go
@@ -1,28 +1,15 @@
 package main
 // could use 'go mod init myapp'
+// go run *.go
 import (
 	"fmt"
 	"net/http"
+	//"html/template"
+	//"errors"
 )
 
 const portNumber = ":8080"
 
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "this is the home page")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	sum := AddValues(2, 2)
-	// fmt.Fprintf(w, "this is the about page")
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2+2 is %d", sum))
-}
-
-// AddValues just add 
-func AddValues(x, y int) int {
-	return x + y
-}
 
 // main just main
 func main() {
