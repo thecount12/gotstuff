4e571137de7e143277bf111f222999d3d6a7180b
William Gunnells <william@thinktankworkspaces.com>
2022-09-20 02:20:33 +0000

    package basics

---
+++ learnpackage/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"learnpackage/simpleinterest"
+)
+
+
+func main() {
+	fmt.Println("simple interest calculation")
+	p := 5000.0
+	r := 10.0
+	t := 1.0
+	si := simpleinterest.Calculate(p, r, t)
+	fmt.Println("Simple interest is", si)
+}
